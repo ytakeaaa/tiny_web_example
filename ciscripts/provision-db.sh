6ac549f28a9ce0308df1b0cebfa82eb219e8e1a8
Masahito Yoshida <masahito@axsh.net>
2015-05-15 07:29:14 +0900

    update db config.

--- ciscripts/provision-db.sh
+++ ciscripts/provision-db.sh
@@ -13,16 +13,15 @@ until mysqladmin -uroot ping; do
   sleep 1
 done
 
-dbname="wordpress"
-dbuser="wordpressuser"
-dbpass="password"
+dbname="tiny_web_example"
+dbuser="root"
 dbacl="10.%"
 
 mysqladmin -uroot drop   ${dbname} <<< "Y" || :
 mysqladmin -uroot create ${dbname} --default-character-set=utf8
 
 mysql -uroot mysql <<EOS
-  GRANT ALL PRIVILEGES ON ${dbname}.* TO ${dbuser}@'${dbacl}' IDENTIFIED BY '${dbpass}';
+  GRANT ALL PRIVILEGES ON ${dbname}.* TO ${dbuser}@'${dbacl}';
   FLUSH PRIVILEGES;
   SELECT * FROM user WHERE User = '${dbuser}' \G
 EOS
