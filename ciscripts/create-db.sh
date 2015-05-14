7517b539b67e59890dc759aa0d5a6b5167894885
Masahito Yoshida <masahito@axsh.net>
2015-05-15 06:05:46 +0900

    add retry.

--- ciscripts/create-db.sh
+++ ciscripts/create-db.sh
@@ -2,7 +2,34 @@
 #
 set -e
 set -o pipefail
-set -x
+set -u
+
+## functions
+
+function retry_until() {
+  local blk="$@"
+
+  local wait_sec=${RETRY_WAIT_SEC:-120}
+  local sleep_sec=${RETRY_SLEEP_SEC:-3}
+  local tries=0
+  local start_at=$(date +%s)
+  local chk_cmd=
+
+  while :; do
+    eval "${blk}" && {
+      break
+    } || {
+      sleep ${sleep_sec}
+    }
+
+    tries=$((${tries} + 1))
+    if [[ "$(($(date +%s) - ${start_at}))" -gt "${wait_sec}" ]]; then
+      echo "Retry Failure: Exceed ${wait_sec} sec: Retried ${tries} times" >&2
+      return 1
+    fi
+    echo [$(date +%FT%X) "#$$"] time:${tries} "eval:${chk_cmd}" >&2
+  done
+}
 
 # setup musselrc
 
@@ -53,4 +80,9 @@ instance_id="$(
   | egrep ^:id: | awk '{print $2}'
 )"
 : "${instance_id:?"should not be empty"}"
+echo "${instance_id} is initializing..." >&2
+
+## wait for the instance to be running
+
+retry_until [[ '"$(mussel instance show "${instance_id}" | egrep -w "^:state: running")"' ]]
 echo instance_id="${instance_id}"
