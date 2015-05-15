3e02a3a64863b4433aaa37a0f9d65bca2978f6a3
Masahito Yoshida <masahito@axsh.net>
2015-05-15 19:39:43 +0900

    use metadata.

--- ciscripts/myaddr.sh
+++ ciscripts/myaddr.sh
@@ -6,4 +6,9 @@ set -e
 set -o pipefail
 set -u
 
-for i in $(/sbin/ip route get 8.8.8.8 | head -1); do echo ipaddr=${i}; done | tail -1
+# ip command:
+# for i in $(/sbin/ip route get 8.8.8.8 | head -1); do echo ipaddr=${i}; done | tail -1
+
+# metadata:
+ipaddr="$(< /metadata/meta-data/local-ipv4)"
+echo ipaddr="${ipaddr}"
