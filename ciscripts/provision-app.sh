fae4c33f31ac0c00ebd307f8b3e6cc0bee823cd9
Masahito Yoshida <masahito@axsh.net>
2015-05-15 09:00:03 +0900

    install epel.

--- ciscripts/provision-app.sh
+++ ciscripts/provision-app.sh
@@ -20,4 +20,15 @@ fi
 sed -i "s,127.0.0.1,${YUM_HOST}," /etc/yum.repos.d/tiny-web-example.repo
 yum repolist
 
+# tiny-web-example depends on epel to install nginx
+rpm -qa epel-release* | egrep -q epel-release || {
+  rpm -Uvh http://ftp.jaist.ac.jp/pub/Linux/Fedora/epel/6/i386/epel-release-6-8.noarch.rpm
+  sed -i \
+   -e 's,^#baseurl,baseurl,' \
+   -e 's,^mirrorlist=,#mirrorlist=,' \
+   -e 's,http://download.fedoraproject.org/pub/epel/,http://ftp.jaist.ac.jp/pub/Linux/Fedora/epel/,' \
+   /etc/yum.repos.d/epel.repo /etc/yum.repos.d/epel-testing.repo
+  yum install -y ca-certificates
+}
+
 yum install -y tiny-web-example
