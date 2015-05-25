f32412e656db70289337c9e42cd4fb0a41fc29c7
Masahito Yoshida <masahito@axsh.net>
2015-05-25 21:46:22 +0900

    update ci script.

--- ciscripts/web3layers-ci.sh
+++ ciscripts/web3layers-ci.sh
@@ -6,19 +6,35 @@ set -o pipefail
 set -u
 set -x
 
+# required shell params
+
+: "${YUM_HOST:?"should not be empty"}"
+
 # run instances
 
 ## db
 
-eval "$(./runner-db.sh)"
+eval "$(${BASH_SOURCE[0]%/*}/runner-db.sh)"
 db_id="${instance_id}"
+DB_HOST="${ipaddr}"
 
 ## app
 
-eval "$(./runner-app.sh)"
+eval "$(
+ YUM_HOST="${YUM_HOST}" \
+  DB_HOST="${DB_HOST}"  \
+  ${BASH_SOURCE[0]%/*}/runner-app.sh
+ )"
 app_id="${instance_id}"
+APP_HOST="${ipaddr}"
+
+# smoketest
+
+## app
+
+APP_HOST="${APP_HOST}" ${BASH_SOURCE[0]%/*}/smoketest-app.sh
 
 # cleanup instances
 
-./instance-kill.sh "${db_id}"
-./instance-kill.sh "${app_id}"
+${BASH_SOURCE[0]%/*}/instance-kill.sh "${db_id}"
+${BASH_SOURCE[0]%/*}/instance-kill.sh "${app_id}"
