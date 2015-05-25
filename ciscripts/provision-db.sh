2c076e0b80dd7b818140c2a13e7b315033da7fbd
Masahito Yoshida <masahito@axsh.net>
2015-05-25 17:34:29 +0900

    use wmi-centos1d64.

--- ciscripts/provision-db.sh
+++ ciscripts/provision-db.sh
@@ -9,19 +9,39 @@ set -o pipefail
 set -u
 set -x
 
+# install mysqld
+yum install -y --disablerepo=updates mysql-server
+
+# configure service
+svc="mysqld"
+chkconfig --list "${svc}"
+chkconfig "${svc}" on
+chkconfig --list "${svc}"
+
+# start mysqld service
+if ! service "${svc}" status; then
+  service "${svc}" start
+fi
+
+# wait for mysqld to be ready
 until mysqladmin -uroot ping; do
   sleep 1
 done
 
+# db params
 dbname="tiny_web_example"
 dbuser="root"
-dbacl="10.%"
+dbacl="10.0.22.%"
 
-mysqladmin -uroot drop   ${dbname} <<< "Y" || :
-mysqladmin -uroot create ${dbname} --default-character-set=utf8
-
-mysql -uroot mysql <<EOS
+# grant db
+mysql -u${dbuser} mysql <<EOS
   GRANT ALL PRIVILEGES ON ${dbname}.* TO ${dbuser}@'${dbacl}';
   FLUSH PRIVILEGES;
   SELECT * FROM user WHERE User = '${dbuser}' \G
 EOS
+
+# create db
+if mysql -u${dbuser} ${dbname} <<< ""; then
+  mysqladmin -u${dbuser} drop ${dbname} <<< "Y"
+fi
+mysqladmin -uroot create ${dbname} --default-character-set=utf8
--- ciscripts/runner-db.sh
+++ ciscripts/runner-db.sh
@@ -19,10 +19,5 @@ eval "$(./instance-get-ipaddr.sh "${instance_id}")"
   ./instance-exec.sh      "${instance_id}" < ./provision-db.sh
 } >&2
 
-{
-  echo generating user_data_app.txt...
-  echo "DB_HOST=${ipaddr}" | tee user_data_app.txt
-} >&2
-
 echo instance_id="${instance_id}"
 echo ipaddr="${ipaddr}"
