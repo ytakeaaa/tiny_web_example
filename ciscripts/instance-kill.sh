447bac3897618e07b9bf5a96ca8b2b027d76a5a8
Masahito Yoshida <masahito@axsh.net>
2015-05-25 19:40:09 +0900

    use retry.sh.

--- ciscripts/instance-kill.sh
+++ ciscripts/instance-kill.sh
@@ -7,32 +7,9 @@ set -e
 set -o pipefail
 set -u
 
-## functions
+## include
 
-function retry_until() {
-  local blk="$@"
-
-  local wait_sec=${RETRY_WAIT_SEC:-120}
-  local sleep_sec=${RETRY_SLEEP_SEC:-3}
-  local tries=0
-  local start_at=$(date +%s)
-  local chk_cmd=
-
-  while :; do
-    eval "${blk}" && {
-      break
-    } || {
-      sleep ${sleep_sec}
-    }
-
-    tries=$((${tries} + 1))
-    if [[ "$(($(date +%s) - ${start_at}))" -gt "${wait_sec}" ]]; then
-      echo "Retry Failure: Exceed ${wait_sec} sec: Retried ${tries} times" >&2
-      return 1
-    fi
-    echo [$(date +%FT%X) "#$$"] time:${tries} "eval:${chk_cmd}" >&2
-  done
-}
+. ${BASH_SOURCE[0]%/*}/retry.sh
 
 ## shell params
 
