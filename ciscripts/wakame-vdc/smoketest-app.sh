6e3e77780a8bec1d8fb1228fd7bb9c503c50b048
Masahito Yoshida <masahito@axsh.net>
2015-05-26 21:01:23 +0900

    remove testing for web.

--- ciscripts/wakame-vdc/smoketest-app.sh
+++ ciscripts/wakame-vdc/smoketest-app.sh
@@ -19,7 +19,3 @@ api_url="http://${APP_HOST}:8080/api/0.0.1/comments"
 curl -fs -X POST --data-urlencode display_name='webapi test' --data-urlencode comment='sample message.' ${api_url}
 curl -fs -X GET ${api_url}
 curl -fs -X GET ${api_url}/1
-
-#
-
-curl -fs -X GET http://${APP_HOST}/
