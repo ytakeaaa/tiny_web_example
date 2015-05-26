2b87b592fdaef58b464fc0812b8ccdef1782d12c
Masahito Yoshida <masahito@axsh.net>
2015-05-26 20:54:50 +0900

    wait for lb-port to be ready.

--- ciscripts/wakame-vdc/runner-lbweb.sh
+++ ciscripts/wakame-vdc/runner-lbweb.sh
@@ -15,7 +15,13 @@ trap "mussel load_balancer destroy \"${load_balancer_id}\"" ERR
 
 eval "$(${BASH_SOURCE[0]%/*}/load_balancer-get-ipaddr.sh "${load_balancer_id}")"
 
+{
+  . ${BASH_SOURCE[0]%/*}/retry.sh
+
+  wait_for_port_to_be_ready "${ipaddr_public}" tcp 80
+} >&2
+
 echo load_balancer_id="${load_balancer_id}"
 echo ipaddr="${ipaddr}"
-echo ipaddr_public="${ipaddr%,*}"
-echo ipaddr_managed="${ipaddr#*,}"
+echo ipaddr_public="${ipaddr_public}"
+echo ipaddr_managed="${ipaddr_managed}"
