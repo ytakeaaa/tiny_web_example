39f4bee262e2582b145f9397e6b25f397326bfa6
Masahito Yoshida <masahito@axsh.net>
2015-05-26 20:26:07 +0900

    add runner-lbapp/lbweb.

--- ciscripts/wakame-vdc/runner-lbapp.sh
+++ ciscripts/wakame-vdc/runner-lbapp.sh
@@ -1,1 +1,21 @@
-create-lbapp.sh
+#!/bin/bash
+#
+#
+set -e
+set -o pipefail
+set -u
+set -x
+
+#
+
+eval "$(
+  ${BASH_SOURCE[0]%/*}/create-lbapp.sh
+  )"
+trap "mussel load_balancer destroy \"${load_balancer_id}\"" ERR
+
+eval "$(${BASH_SOURCE[0]%/*}/load_balancer-get-ipaddr.sh "${load_balancer_id}")"
+
+echo load_balancer_id="${load_balancer_id}"
+echo ipaddr="${ipaddr}"
+echo ipaddr_public="${ipaddr%,*}"
+echo ipaddr_managed="${ipaddr#*,}"
