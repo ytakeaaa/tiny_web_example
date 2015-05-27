8a1c15fda165719bfdc12dff7c82dac6c0f75fda
Masahito Yoshida <masahito@axsh.net>
2015-05-27 16:44:27 +0900

    enable to set specified app host.

--- ciscripts/integration-test.sh
+++ ciscripts/integration-test.sh
@@ -9,4 +9,9 @@ cd spec_integration
 
 bundle install
 cp config/webapi.conf.example config/webapi.conf
+
+if [[ -n "${APP_HOST:-""}" ]]; then
+  sed -i s,localhost,${APP_HOST}, config/webapi.conf
+fi
+
 bundle exec rspec ./spec/webapi_integration_spec.rb
