aa9088ed49d29196d7e10d1612e61efd63d81d23
ytakeaaa <y.takeuchi_2@jp.fujitsu.com>
2015-06-02 18:49:14 +0900

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
 #
+echo "--- start ---"
+
 set -e
 set -o pipefail
 set -u
@@ -22,7 +24,18 @@ cd ciscripts
 
 export IMAGE_ID=wmi-centos1d64
 export YUM_HOST=10.0.22.102
-export WRITE_FILE=imagefile.txt
+export WRITE_FILE=/var/lib/jenkins/jobs/buildtest/workspace/imagefile.txt
 
 echo "--- image build ---"
 bash ./image-build.sh
+
+image_ids=($(cat $WRITE_FILE))
+export DB_IMAGE_ID=${image_ids[0]}
+export APP_IMAGE_ID=${image_ids[1]}
+echo DB_IMAGE_ID=$DB_IMAGE_ID
+echo APP_IMAGE_ID=$APP_IMAGE_ID
+
+echo "--- web 3 layer ci ---"
+bash ./web3layers-ci.sh
+
+echo "--- end ---"
